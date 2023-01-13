2f6a0d800959f634709705a4c2dccf61b45dbfc1
I0x0I <ivan.i0x0i@gmail.com>
2023-01-13 15:35:34 -0600

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,5 +2,10 @@
 
 DIR="$( cd "$( dirname "$0" )" && pwd )"
 
-cd $DIR/build/ && ./z1_ctrl
+if [ ! -d "$DIR/build" ]
+then
+  mkdir $DIR/build
+  cd $DIR/build && cmake ../ && make
+fi
 
+cd $DIR/build/ && ./z1_ctrl
